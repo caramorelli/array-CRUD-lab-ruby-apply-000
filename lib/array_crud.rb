32ca3cac0ac8dd37fdb7e99212a43e45fdf47417
Cara Morelli <caramorelli7@gmail.com>
2018-09-10 16:53:24 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,12 +10,12 @@ end
 
 def add_element_to_end_of_array(array, element)
   array << element
-  array
+  # array
 end
 
 def add_element_to_start_of_array(array, element)
   array.unshift(element)
-  array
+  # array
 end
 
 def remove_element_from_end_of_array(array)
